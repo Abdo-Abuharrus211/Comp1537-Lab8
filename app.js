b31066f54d73862b947d6a854ea3d3eb4490934a
Abdulqadir_Abuharrus <aabuharrus@gmail.com>
2023-02-23 12:53:55 -0800

    Add styles.css & modify app.js

--- app.js
+++ app.js
@@ -7,33 +7,36 @@ function compute(operation) {
 
         case "add":
             console.log(op1 + op2)
-            $("#result").text(`Result is ${op1 + op2}`);
+            $("#result").text(`Result of ${op1} + ${op2} = ${op1 + op2}`);
             break;
 
         case "sub":
             console.log(op1 - op2)
-            $("#result").text(`Result is ${op1 + op2}`);
+            $("#result").text(`Result of ${op1} - ${op2} = ${op1 - op2}`);
             break;
 
         case "multi":
             console.log(op1 * op2)
-            $("#result").text(`Result is ${op1 + op2}`);
+            $("#result").text(`Result of ${op1} * ${op2} = ${op1 * op2}`);
             break;
 
         case "divide":
             console.log(op1 / op2)
-            $("#result").text(`Result is ${op1 + op2}`);
+            $("#result").text(`Result of ${op1} / ${op2} = ${op1 / op2}`);
             break;
 
         case "power":
             console.log(op1 ** op2)
-            $("#result").text(`Result is ${op1 + op2}`);
+            $("#result").text(`Result of ${op1} to the power ${op2} = ${op1 ** op2}`);
             break;
 
         default:
             result = "NaN";
 
     }
+    $("#history").append(`<div class="${operation}">${$("#result").text()}
+    <button class="remove-btn">Hide</button>
+    </div>`)
 }
 
 
@@ -41,7 +44,12 @@ const setup = () => {
     $("button").click(function () {
         compute(this.id);
     });
+
+    $("body").on("click", ".remove-btn", function() {
+        $(this).parent().remove()
+    });
 }
+
 
 
 $(document).ready(setup);
